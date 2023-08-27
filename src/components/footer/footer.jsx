cd9706fe6a8692174feafd3ccfcdea8db98937fc
Alexis Garcia <alexis25yo@gmail.com>
2023-08-27 15:58:33 -0500

    listo las ordenes y el detalle

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,22 +2,55 @@
 import React from "react";
 import "./footer.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter } from "next/navigation";
 
 const Footer = () => {
-  const currentPath = usePathname()
-  console.log("la ruta",currentPath)
-  const router = useRouter()
-  const goHome = ()=>{
-    router.push('/')
-  }
+  const currentPath = usePathname();
+  const router = useRouter();
+
+  const goRute = (page) => {
+    if (page == "home") {
+      router.push("/home");
+    }
+    if (page == "search") {
+      router.push("/search");
+    }
+    if (page == "order") {
+      router.push("/order");
+    }
+    if (page == "perfil") {
+      console.log("voy para", page);
+    }
+  };
+
   return (
     <div className="footerPrimary">
-      {currentPath=='/home'?<i className="bi bi-house-door text-warning" onClick={goHome}></i>:<i className="bi bi-house-door" onClick={goHome}></i>}
-      
-      <i className="bi bi-search"></i>
-      <i className="bi bi-stopwatch"></i>
-      <i className="bi bi-person"></i>
+      <i
+        className={`bi bi-house-door ${
+          currentPath === "/home" ? "text-warning puntito" : ""
+        }`}
+        onClick={() => goRute("home")}
+      ></i>
+      <i
+        className={`bi bi-search ${
+          currentPath === "/search" ? "text-warning puntito" : ""
+        }`}
+        onClick={() => goRute("search")}
+      ></i>
+      <i
+        className={`bi bi-stopwatch  ${
+          currentPath === "/order" || currentPath === "/detail"
+            ? "text-warning puntito"
+            : ""
+        }`}
+        onClick={() => goRute("order")}
+      ></i>
+      <i
+        className={`bi bi-person  ${
+          currentPath === "/perfil" ? "text-warning puntito" : ""
+        }`}
+        onClick={() => goRute("perfil")}
+      ></i>
     </div>
   );
 };
