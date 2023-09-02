c3a3001e8ca15e3ae4da9db104bee4fea6d6b76b
sergiotechx <sergiotechx@yahoo.com>
2023-09-02 16:36:51 -0500

    store restaurants, restaurantTypes

--- src/store/restaurantTypes/restaurantTypesSlice.js
+++ src/store/restaurantTypes/restaurantTypesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialValue = {
-    restaurantTypes: [],
+    restaurantTypes_: [],
     error: null
     
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
 import {orderSlice} from './order/orderReducer'
+import{restaurantTypesSlice} from './restaurantTypes/restaurantTypesSlice'
+import {restaurantsSlice} from './restaurants/restaurantsSlice'
 import thunk from "redux-thunk";
 
 
 const rootReducer = {
   auth: authSlice.reducer,
-  order: orderSlice.reducer
+  order: orderSlice.reducer,
+  restaurantTypes:restaurantTypesSlice.reducer,
+  restaurants:restaurantsSlice.reducer
   // ...otros reducers si los tienes
 };
 
