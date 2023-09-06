fc0163cd6bd466efc3d157d85744b49b2839251e
sergiotechx <sergiotechx@yahoo.com>
2023-09-05 23:43:54 -0500

    arreglos perfil en if

--- src/app/(pages)/user/perfil/page.jsx
+++ src/app/(pages)/user/perfil/page.jsx
@@ -5,7 +5,7 @@ import "./perfil.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
-import { starLogout } from "@/store/auth/thunks";
+import { starLogout } from "../../../../store/auth/thunks";
 import Swal from "sweetalert2";
 
 const Page = () => {
@@ -14,70 +14,71 @@ const Page = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.auth);
 
-    
+
     const isLogout = useMemo(() => status === "not-authenticated", [status]);
 
-    const goToEdit = () =>{
+    const goToEdit = () => {
         router.push('perfil-edit')
     }
-    const goToPayment = () =>{
+    const goToPayment = () => {
         router.push('perfil-payment')
     }
 
     const logout = () => {
-       dispatch( starLogout() );
-       Swal.fire("Oops", "debes iniciar sesion para permanecer en perfil", "error");
+        dispatch(starLogout());
+        Swal.fire("Oops", "debes iniciar sesion para permanecer en perfil", "error");
+        if (isLogout) {
+            router.push("/home");
+        }
     }
-    if (isLogout) { 
-        router.push("/home");
-      }
-  return (
-    <div>
+
+    return (
         <div>
-            <figure className='image'>
-                <img src={user.photoURL} alt="" />
-                <h5>{user.displayName}</h5>
-            </figure>
+            <div>
+                <figure className='image'>
+                    <img src={user.photoURL} alt="" />
+                    <h5>{user.displayName}</h5>
+                </figure>
+            </div>
+            <div className='options'>
+                <span className='option'>
+                    <i onClick={goToEdit} className="bi bi-person l"></i>
+                    <p onClick={goToEdit}>Account edit</p>
+                    <i onClick={goToEdit} className="bi bi-chevron-right r"></i>
+                </span>
+                <span className='option'>
+                    <i className="bi bi-bell l"></i>
+                    <p>Notifications</p>
+                    <i className="bi bi-chevron-right r"></i>
+                </span>
+                <span className='option'>
+                    <i onClick={goToPayment} className="bi bi-credit-card l"></i>
+                    <p onClick={goToPayment} >Payment</p>
+                    <i onClick={goToPayment} className="bi bi-chevron-right r"></i>
+                </span>
+                <span className='option'>
+                    <i className="bi bi-globe l"></i>
+                    <p>Language</p>
+                    <i className="bi bi-chevron-right r"></i>
+                </span>
+                <span className='option'>
+                    <i className="bi bi-geo-alt l"></i>
+                    <p>Location</p>
+                    <i className="bi bi-chevron-right r"></i>
+                </span>
+                <span className='option'>
+                    <i className="bi bi-question-circle l"></i>
+                    <p>FAQ</p>
+                    <i className="bi bi-chevron-right r"></i>
+                </span>
+                <span className='option'>
+                    <i onClick={logout} className="bi bi-x-square l"></i>
+                    <p onClick={logout}>Close Session</p>
+                    <i onClick={logout} className="bi bi-chevron-right r"></i>
+                </span>
+            </div>
         </div>
-        <div className='options'>
-            <span className='option'>
-            <i onClick={goToEdit} className="bi bi-person l"></i>
-            <p onClick={goToEdit}>Account edit</p>
-            <i onClick={goToEdit} className="bi bi-chevron-right r"></i>
-            </span>
-            <span className='option'>
-            <i className="bi bi-bell l"></i>
-            <p>Notifications</p>
-            <i className="bi bi-chevron-right r"></i>
-            </span>
-            <span className='option'>
-            <i onClick={goToPayment} className="bi bi-credit-card l"></i>
-            <p onClick={goToPayment} >Payment</p>
-            <i onClick={goToPayment} className="bi bi-chevron-right r"></i>
-            </span>
-            <span className='option'>
-            <i className="bi bi-globe l"></i>
-            <p>Language</p>
-            <i className="bi bi-chevron-right r"></i>
-            </span>
-            <span className='option'>
-            <i className="bi bi-geo-alt l"></i>
-            <p>Location</p>
-            <i className="bi bi-chevron-right r"></i>
-            </span>
-            <span className='option'>
-            <i className="bi bi-question-circle l"></i>
-            <p>FAQ</p>
-            <i className="bi bi-chevron-right r"></i>
-            </span>
-            <span className='option'>
-            <i onClick={logout} className="bi bi-x-square l"></i>
-            <p onClick={logout}>Close Session</p>
-            <i onClick={logout} className="bi bi-chevron-right r"></i>
-            </span>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default Page;
